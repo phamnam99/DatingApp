9014ede43326c283796def4c68892532e4e38066
nam phạm <phamnam101299@gmail.com>
2025-03-06 22:17:19 +0700

    First commit

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<AppUser> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            return user;
+        }
+    }
+}
